b4e7efad0381de44e03e224328a7ae0244ac9d31
rick walker <=>
2016-08-27 11:42:41 -0400

    Added test case method that tests passing a null argument to the ElementGenerator's makePage method

--- htmlgenerator/src/test/java/com/gmail/ichglauben/pagegenerator/ElementGeneratorTests.java
+++ htmlgenerator/src/test/java/com/gmail/ichglauben/pagegenerator/ElementGeneratorTests.java
@@ -128,4 +128,67 @@ public class ElementGeneratorTests extends CustomClass {
 		TextfileOverwriter.overwrite(ud + "log2", newPage, ".html");
 	}
 
+	@Test
+	public void testBootStrapPageGeneratorNullArgument() throws IOException {
+		gen = new ElementGenerator();
+		
+		// head elements
+		String[] headElements = new String[] {
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">",
+				"<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>",
+				"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>",
+				"<title>HTML Generator</title>"
+		};		
+
+		// navigation bar		
+		String nav = "<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">" +
+						"<div class=\"container-fluid\">" +
+							"<div class=\"navbar-header\">" +
+								"<span class=\"navbar-brand\" id=\"navbarheadertop\">Report</span>" +
+							"</div>" +
+							"<ul class=\"nav navbar-nav\">" +
+								"<li><a id=\"about\" class=\"noanchor\" href=\"#\">About</a></li>" +
+								"<li><a id=\"map\" class=\"anchor\" href=\"map.html\">Map</a></li>" +
+							"</ul>" +					
+						"</div>" +
+					"</nav>";
+		
+		// main content
+		String content = "<main class=\"content\" id=\"content\">" +
+				"<div class=\"row\">" +	
+					"<div class=\"col-sm-3\">" +
+						"<div class=\"jumbotron\"></div>" +
+					"</div>" +
+					"<div class=\"col-sm-9\">" +
+						"<div class=\"jumbotron\"></div>" +
+					"</div>" +
+				"</div>";		
+		
+		// bottom script and link tags
+		String[] bottomElements = new String[] {
+				"<link rel=\"stylesheet\" href=\"https://dl.dropboxusercontent.com/u/50203839/web/sites/quauab/css/style_a.css\">"				
+		};
+		
+		// body tag settings
+		List<String> bodySettings = new ArrayList<String>();
+			bodySettings.add("class=\"container\"");
+		
+		// body child elements
+		String bodyElements[] = new String[] {nav,content};
+		
+		// page components
+		String head = gen.makeHead(headElements);
+		String body = gen.makeBody(bodySettings,bodyElements);
+		String page = gen.makePage(head, body, null);
+		
+		String[]pageB = page.split("<br>");
+		List<String> newPage = new ArrayList<String>();
+		
+		for (String p:pageB) {
+			println(p);
+			newPage.add(p);
+		}
+		
+		TextfileOverwriter.overwrite(ud + "log3", newPage, ".html");
+	}
 }
