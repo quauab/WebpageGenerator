378376922d663e8563f0644f4235b3d908c1208d
rick walker <=>
2016-08-28 22:28:28 -0400

    Added another build method to WebpageGeneratorHelper and its corresponding test case

--- htmlgenerator/src/main/java/com/gmail/ichglauben/pagegenerator/core/WebpageGeneratorHelper.java
+++ htmlgenerator/src/main/java/com/gmail/ichglauben/pagegenerator/core/WebpageGeneratorHelper.java
@@ -21,5 +21,20 @@ public class WebpageGeneratorHelper {
 		return page;
 	}
 	
+	/**Returns page String array The built web page.
+	 * @param pageHead String array The head tag's children elements
+	 * @param bodySettings String array The body tag's property settings
+	 * @param content String array The body's children elements
+	 * @param bottomElements String array html tags inserted before the end html tag*/
+	public String[] buildAndReturnPage(String[] pageHead, String[] bodySettings, String[] content, String[] bottomElements) {		
+		String head = wg.makeHead(pageHead);
+		
+		String body = wg.makeBody(bodySettings, content);
+		
+		String page = wg.makePage(head, body, bottomElements);
+
+		return page.split("<br>");
+	}
+	
 	public String toString() { return "Webpage Generator's Helper Class"; }
 }
